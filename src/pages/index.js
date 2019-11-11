ebe3dfbe521a76c5fd9ef416900140d727741a97
Adrian Mato <yensamg@gmail.com>
2019-11-10 23:13:45 -0800

    Add: home page description

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,6 @@ import React from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
 
@@ -10,13 +9,16 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <h1>Adrián Mato</h1>
+    <h2>Designing <span class="ampersand">&amp;</span> building tools for developers at GitHub.</h2>
+    <p>
+      For the last 12+ years, I've designed digital products between Madrid, London and now, from
+      San Francisco. I also co-founded and later sold my company Erasmusu.
+    </p>
+    <p>
+      My career started as a Software Developer but it soon became clear that my true passion was Design.
+      I love combining both skillsets to create products that fulfill a clear purpose.
+    </p>
   </Layout>
 )
 
--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -3,12 +3,16 @@ import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import Image from "../components/image"
 
 const SecondPage = () => (
   <Layout>
     <SEO title="Page two" />
     <h1>Hi from the second page</h1>
     <p>Welcome to page 2</p>
+    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+      <Image />
+    </div>
     <Link to="/">Go back to the homepage</Link>
   </Layout>
 )
