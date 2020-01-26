6d4f042993f93757bbeb70bddf5dbbe325003aea
Adrian Mato <yensamg@gmail.com>
2020-01-26 01:09:51 -0800

    Data fetch from config file

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,9 @@
 // TODO: Review what dependencies are needed or not in `package.json`
 import React from 'react';
+import { graphql } from 'gatsby';
 import styled from '@emotion/styled';
-import { Link } from 'gatsby';
 import Layout from '../components/Layout';
+import Navigation from '../components/IndexNavigation';
 import SEO from '../components/SEO';
 
 // styled components
@@ -16,51 +17,62 @@ const Subtitle = styled.h2`
 `;
 const Bio = styled.p``;
 
-const Navigation = styled.nav``;
+export interface IndexPageProps {
+  data: {
+    site: {
+      siteMetadata: {
+        title: string;
+        titleDefault?: string;
+        description: string;
+        email: string;
+        social: string;
+      };
+    };
+  };
+}
 
-const IndexPage = () => (
-  <Layout theme="">
-    <SEO title="false" />
-    <Title>Adrián Mato</Title>
-    <Subtitle>
-      Designing <span className="ampersand">&amp;</span> building tools for
-      developers at GitHub.
-    </Subtitle>
-    <Bio>
-      For the last 12+ years, I&apos;ve designed digital products between
-      Madrid, London and now, from San Francisco. I also co-founded and later
-      sold my company Erasmusu.
-    </Bio>
-    <Bio>
-      My career started as a Software Developer but it soon became clear that my
-      true passion was Design. I love combining both skillsets to create
-      products that fulfill a clear purpose.
-    </Bio>
-    {/* TODO: Put this inside a component? */}
-    <Navigation>
-      <ul>
-        {/* TODO: grab values from configuration of the site */}
-        <li>
-          <a href="#home-work">Portfolio</a>
-        </li>
-        <li>
-          <Link to="/blog">Blog</Link>
-        </li>
-        <li>
-          <a href="https://linkedin.com/in/adrianmg">Resume</a>
-        </li>
-        <li>
-          <a href="https://github.com/adrianmg">GitHub</a>
-        </li>
-        <li>
-          <a href="https://twitter.com/adrianmg">Twitter</a>
-        </li>
-        <li>
-          <a href="mailto:hello@adrianmato.com">Contact</a>
-        </li>
-      </ul>
-    </Navigation>
-  </Layout>
-);
+const IndexPage = ({ data }: IndexPageProps) => {
+  const view = data.site.siteMetadata;
+  return (
+    <Layout theme="">
+      <SEO
+        title={view.titleDefault ? view.titleDefault : view.title}
+        description={view.description}
+      />
+      <Title>{view.title}</Title>
+      <Subtitle>
+        Designing <span className="ampersand">&amp;</span> building tools for
+        developers at GitHub.
+      </Subtitle>
+      <Bio>
+        For the last 12+ years, I&apos;ve designed digital products between
+        Madrid, London and now, from San Francisco. I also co-founded and later
+        sold my company Erasmusu.
+      </Bio>
+      <Bio>
+        My career started as a Software Developer but it soon became clear that
+        my true passion was Design. I love combining both skillsets to create
+        products that fulfill a clear purpose.
+      </Bio>
+      <Navigation social={view.social} email={view.email} />
+
+      <div id="work"></div>
+    </Layout>
+  );
+};
 
 export default IndexPage;
+
+export const query = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+        titleDefault
+        description
+        email
+        social
+      }
+    }
+  }
+`;
