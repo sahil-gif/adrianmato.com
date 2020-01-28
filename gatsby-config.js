4ce89697ab29edcb26b9e4108b996869ae18d83a
Adrian Mato <yensamg@gmail.com>
2020-01-27 22:04:44 -0800

    Update: manifesto config

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,8 +28,8 @@ module.exports = {
         name: `gatsby-starter-default`,
         short_name: `starter`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#12121a`,
+        theme_color: `#ffb87a`,
         display: `minimal-ui`,
         icon: `src/images/favicon.png`, // This path is relative to the root of the site.
       },
